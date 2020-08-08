a08a57e82222e0136568378cadf93972cfa03764
Kevin Tong <kevinsomnia17@gmail.com>
2020-08-08 17:24:16 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const md5 = require('md5');
+const app = express();
+
+const PORT = 80;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    if (req.query.hasOwnProperty('url')) {
+        (async () => {
+            let url = req.query['url'];
+            let hash = md5(url);
+            let downloadPath = path.join(__dirname, `public/downloads/${hash}.jpg`);
+
+            await downloadImage(url, downloadPath).then(() => {
+                res.sendFile(downloadPath);
+                console.log(`Download finished for ${url}! Sent file: ${downloadPath}`);
+            }).catch(() => {
+                console.log(`Error: ${url} failed to load`);
+                res.status(500).send();
+            });
+        })();
+    }
+    else {
+        res.status(400).send();
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Started server on port ${PORT}`);
+});
+
+async function downloadImage (url, path) {
+    if (!fs.existsSync(path)) {
+        // Start downloading.
+        const response = await axios({
+            url,
+            method: 'GET',
+            responseType: 'stream'
+        });
+
+        if (response.status != 200) {
+            return new Promise((_, reject) => reject());
+        }
+    
+        const writer = fs.createWriteStream(path);
+        response.data.pipe(writer);
+        
+        // Return promise that writes to the target file.
+        return new Promise((resolve, reject) => {
+            writer.on('finish', () => resolve())
+            writer.on('error', e => {
+                reject(e);
+            });
+        });
+    }
+    else {
+        return new Promise((resolve) => resolve());
+    }
+}
